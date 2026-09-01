1b3f56a74dd4be071ce25da6aa4238ba6bcf63b7
achil-hue <chary.achilov@gmail.com>
2026-09-01 21:14:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def start_health_check_server():
 
 # Прямое подключение к Binance без прокси
 exchange = ccxt.binance({'enableRateLimit': True})
-MIN_DROP_PERCENT = 2.00
-MAX_DROP_PERCENT = 5.0
+MIN_DROP_PERCENT = 1.00
+MAX_DROP_PERCENT = 5.00
 CHECK_INTERVAL = 60
 LOOKBACK_MINUTES = 15
 
